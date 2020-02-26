e6f0bad9136f8c14597a66f84ee20d616b0cf15f
flynxlog <admin@flynxlogistics.com>
2020-02-25 19:32:59 -0500

    added security group for ingress

--- main.tf
+++ main.tf
@@ -9,5 +9,19 @@ resource "aws_instance" "example" {
   tags = {
     Name = "terraform-example"
   }
+
+  
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
   
 }
